70aa663122825b59e16e1a4015e898371b03b065
AlbertFalzon <73096411+AlbertFalzon@users.noreply.github.com>
2021-02-25 22:30:22 +0100

    added skybox, lighting, terrain, ship, script to move terrain texture and to rotate ship

---
+++ RaceTheSun/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float turnSpeed = 5f;
+    float maxCW = -135f;
+    float maxCCW = -45f;
+
+    void Update()
+    {
+        PlayerRotate();
+    }
+
+    private void PlayerRotate()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            var targetAngle = Quaternion.Euler(-135f, -90f, 90f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetAngle, turnSpeed * Time.deltaTime);
+        } else if (Input.GetAxis("Horizontal") < 0)
+        {
+            var targetAngle = Quaternion.Euler(-45f, -90f, 90f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetAngle, turnSpeed * Time.deltaTime);
+        } else
+        {
+            var targetAngle = Quaternion.Euler(-90f, -90f, 90f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetAngle, (turnSpeed / 5) * Time.deltaTime);
+        }
+
+    }
+}
---
+++ RaceTheSun/Assets/Scripts/TerrainScroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainScroll : MonoBehaviour
+{
+    [SerializeField] float scrollSpeed = 0.5f;
+    Material terrain;
+    Vector2 offset;
+    
+    void Start()
+    {
+        terrain = GetComponent<Renderer>().material;
+        offset = new Vector2(0f, scrollSpeed);
+    }
+
+    void Update()
+    {
+        terrain.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
